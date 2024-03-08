f3f5e15acf7132d5f9a6643efbb2e9bd8ba0df95
Ferran Bals - iMac <ferranbals@gmail.com>
2024-03-08 18:42:56 +0100

    Índice autogenerado con el contenido del tag main

---
+++ content.js
@@ -0,0 +1,30 @@
+/*import apuntes from "./apuntes.json" assert { type: "json" };
+
+console.log(apuntes);
+*/
+
+// Obtenemos los artículos del DOM
+const articles = document.querySelectorAll("#apuntes article");
+
+// Obtenemos el elemento donde queremos colocar el índice
+const nav = document.getElementById("index");
+
+// Obtenemos las temáticas y los títulos de las sesiones de cada temática para inyectarlos al índice
+
+articles.forEach((article) => {
+  const heading = article.querySelector("h2").innerText;
+  const listItem = document.createElement("li");
+  listItem.textContent = heading;
+
+  const subList = document.createElement("ul");
+
+  const subHeadings = article.querySelectorAll("h3");
+  subHeadings.forEach((subHeading) => {
+    const subListItem = document.createElement("li");
+    subListItem.textContent = subHeading.textContent;
+    subList.appendChild(subListItem);
+  });
+
+  listItem.appendChild(subList);
+  nav.appendChild(listItem);
+});
